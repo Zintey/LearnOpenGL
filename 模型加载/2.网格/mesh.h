9e541672a7a10f9299aef75edaf5cee42ab4fa31
z1687019603 <1687019603@qq.com>
2026-06-04 17:30:41 +0800

    网格

---
+++ 模型加载/2.网格/mesh.h
@@ -0,0 +1,89 @@
+#pragma once
+#include <glad/glad.h>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
+#include <string>
+#include <shader.h>
+
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+struct Texture {
+	unsigned int id;
+	std::string type;
+};
+
+class Mesh {
+public:
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures)
+		: vertices(std::move(vertices)), indices(std::move(indices)), textures(std::move(textures)) 
+	{
+		setupMesh();
+	}
+
+	void Draw(Shader& shader)
+	{
+		shader.use();
+		unsigned int diffuseNr = 1;
+		unsigned int specularNr = 1;
+		//unsigned int normalNr = 1;
+		//unsigned int heightNr = 1;
+		for (int i = 0; i < textures.size(); i++)
+		{
+			glActiveTexture(GL_TEXTURE0 + i);
+			std::string name = textures[i].type;
+			std::string number;
+			if (textures[i].type == "texture_diffuse")
+				number = std::to_string(diffuseNr++);
+			else if (textures[i].type == "texture_specular")
+				number = std::to_string(specularNr++);
+			//else if (textures[i].type == "texture_normal")
+				//number = std::to_string(normalNr++);
+			//else if (textures[i].type == "texture_height")
+				//number = std::to_string(heightNr++);
+			shader.setInt(("material." + name + number).c_str(), i);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+		}
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(indices.size()), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		glActiveTexture(GL_TEXTURE0);
+	}
+private:
+	unsigned int VAO, VBO, EBO;
+	void setupMesh()
+	{
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+		// Position		位置
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) 0);
+		// Normal		法线
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+		// TexCoords	uv坐标
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+		
+		glBindVertexArray(0);
+	}
+};
